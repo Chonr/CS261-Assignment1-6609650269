44ff63754d7a886b7ef2e73dbd45fff81991fdec
Chonrathan Sinlapachan <namchonrathan@gmail.com>
2024-10-09 18:41:25 +0700

    eighth commit: server.js

--- server.js
+++ server.js
@@ -9,5 +9,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'login.html'));
 });
 
+app.get('/hello', (req, res) => {
+    const { myName, lastName } = req.query; // Get query parameters
+    res.send(`Hello ${myName} ${lastName}`); // Return greeting message
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
