c96c709406e03765c754754359363b103412850d
Chonrathan Sinlapachan <namchonrathan@gmail.com>
2024-10-09 17:00:22 +0700

    fourth commit: JS

--- public/js/script.js
+++ public/js/script.js
@@ -1,35 +1,39 @@
 function submitLogin() {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
+    const role = document.getElementById('role').value;
 
-    fetch('/api/auth', {
+    fetch('https://restapi.tu.ac.th/api/v1/auth/Ad/verify', {
         method: 'POST',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Application-Key': 'TU5ce4207d6fb3085aba32c5a74e72aa711e9e07ad870eb799d80eb6330f460223e6ddf0d1dabcfca1cf64daecc8900a42'
         },
-        body: JSON.stringify({ username, password })
+        body: JSON.stringify({ "Username" : username, "Password" : password })
     })
     .then(response => response.json())
     .then(data => {
         document.getElementById('message').innerText = data.message;
     })
     .catch(error => console.error('Error:', error));
+
 }
 
-
-
+// Existing function for calling REST API
 function call_REST_API_Hello() {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
+
     const url = (
-        'http://localhost:8080/hello?' +
-        new URLSearchParams({ myName: username, lastName: password}).toString()
-      );
+        'http://localhost:3000/hello?' + // Change port if needed
+        new URLSearchParams({ myName: username, lastName: password }).toString()
+    );
     
     fetch(url)
-    .then(data => {
-        document.getElementById('message').innerText = data.message;
+    .then(response => response.text())
+    .then(text => {
+        document.getElementById('message').innerText = text;
     })
     .catch(error => console.error('Error:', error));
 }
