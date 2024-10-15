8b26fbf4849a029f51bbbafe441f504d84e0002b
Chonrathan Sinlapachan <namchonrathan@gmail.com>
2024-10-15 17:59:00 +0700

    20th commit: remove function call_REST_API_Hello()

--- public/js/script.js
+++ public/js/script.js
@@ -37,28 +37,6 @@ function submitLogin() {
 
 }
 
-function call_REST_API_Hello() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    if (!username || !password) {
-        alert("Please fill in both username and password."); 
-        return; 
-    }
-
-    const url = (
-        'http://localhost:3000/hello?' + 
-        new URLSearchParams({ myName: username, lastName: password }).toString()
-    );
-    
-    fetch(url)
-    .then(response => response.text())
-    .then(text => {
-        document.getElementById('message').innerText = text;
-    })
-    .catch(error => console.error('Error:', error));
-}
-
 function requestForm() {
     location.reload();
 }
