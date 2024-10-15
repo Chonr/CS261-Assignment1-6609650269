a722f251b178849578be15c6a17855f2aac7b1b4
Chonrathan Sinlapachan <namchonrathan@gmail.com>
2024-10-15 21:32:58 +0700

    23th commit: remove hello

--- server.js
+++ server.js
@@ -5,10 +5,5 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/hello', (req, res) => {
-    const { myName, lastName } = req.query; // Get query parameters
-    res.send(`Hello ${myName} ${lastName}`); // Return greeting message
-});
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
